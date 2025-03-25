71949499c90f05cb555e82c7ea11d55d99ba38ed
Rohit Rathore <rohitrathore564025@gmail.com>
2025-03-25 12:26:40 +0530

    Remove the react native share lib and Solve the issue of All file access

--- src/Screens/Feedback/ViewFeedback.tsx
+++ src/Screens/Feedback/ViewFeedback.tsx
@@ -6,21 +6,23 @@ import {
   TouchableOpacity,
   ScrollView,
   ActivityIndicator,
+  Alert,
 } from 'react-native';
-import React, {useEffect, useState} from 'react';
-import {useSelector} from 'react-redux';
-import {isDarkTheme} from '../../AppStore/Reducers/appState';
-import {Colors} from '../../constants/Colors';
+import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { isDarkTheme } from '../../AppStore/Reducers/appState';
+import { Colors } from '../../constants/Colors';
 import {
   useGetAttachmentFromSharePointQuery,
   useGetMyFeedbacksByFeedBackIdQuery,
 } from '../../Services/services';
-import {IconButton} from 'react-native-paper';
+import { IconButton } from 'react-native-paper';
 import moment from 'moment';
 import Share from 'react-native-share';
 import CustomHeader from '../../Components/CustomHeader';
+import RNFS from 'react-native-fs';
 
-const ViewFeedback = ({route,navigation}: any) => {
+const ViewFeedback = ({ route, navigation }: any) => {
   const { feedbackData } = route.params;
   const isDark = useSelector(isDarkTheme);
   const EmployeeId = useSelector((state: any) => state?.appState?.authToken);
@@ -31,7 +33,7 @@ const ViewFeedback = ({route,navigation}: any) => {
     FeedBackId: feedbackData?.feedBackId,
     accessToken: EmployeeId?.authToken?.accessToken,
   };
-  const {data, refetch, error} = useGetMyFeedbacksByFeedBackIdQuery(params);
+  const { data, refetch, error } = useGetMyFeedbacksByFeedBackIdQuery(params);
 
   useEffect(() => {
     if (data) {
@@ -39,14 +41,30 @@ const ViewFeedback = ({route,navigation}: any) => {
     }
   }, [data]);
 
-  const {data: attachmentData, isLoading} = useGetAttachmentFromSharePointQuery(
+  const { data: attachmentData, isLoading } = useGetAttachmentFromSharePointQuery(
     {
       entityId: feedbackData?.feedBackId,
       entityName: 'solz_feedback',
       accessToken: EmployeeId?.authToken?.accessToken,
     },
   );
-  
+
+
+  const handleDownload = async (base64Data: string | null, fileName: string) => {
+    if (!base64Data) {
+      Alert.alert('Download failed', 'No file data available.');
+      return;
+    }
+
+    try {
+      const downloadPath = `${RNFS.DownloadDirectoryPath}/${fileName}`;
+      await RNFS.writeFile(downloadPath, base64Data, 'base64');
+      Alert.alert('Download Success', `File saved to: ${downloadPath}`);
+    } catch (error) {
+      console.log('File Download Failed:', error);
+      Alert.alert('Download failed', 'There was an error while downloading the file.');
+    }
+  };
 
   if (!feedbackData) {
     return (
@@ -64,25 +82,6 @@ const ViewFeedback = ({route,navigation}: any) => {
     }, 1000);
   };
 
-  const base64ToUri = (base64String: string | null, mimeType: string) => {
-    return `data:${mimeType};base64,${base64String}`;
-  };
-
-  const downloadFile = async (base64Data: string | null, fileName: string) => {
-    Share.open({
-      url: base64ToUri(base64Data, 'application/pdf'),
-      type: 'application/pdf',
-      title: fileName,
-      message: fileName,
-      subject: fileName,
-    })
-      .then(res => {
-        console.log(res);
-      })
-      .catch(err => {
-        err && console.log(err);
-      });
-  };
 
   return (
     <View style={styles(isDark).mainContainer}>
@@ -93,7 +92,7 @@ const ViewFeedback = ({route,navigation}: any) => {
       />
       <View style={styles(isDark).divider} />
       {isLoading ? (
-        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
           <ActivityIndicator size="large" color={Colors.primary} />
         </View>
       ) : (
@@ -120,21 +119,21 @@ const ViewFeedback = ({route,navigation}: any) => {
             )}
           </Text>
 
-          <View style={{marginHorizontal: 16}}>
-            <Text style={[styles(isDark).txt, {flex: 1, flexWrap: 'wrap'}]}>
+          <View style={{ marginHorizontal: 16 }}>
+            <Text style={[styles(isDark).txt, { flex: 1, flexWrap: 'wrap' }]}>
               {[feedbackByID[0]?.feedBackTitle]}
             </Text>
           </View>
-          
-          <View style={{marginHorizontal: 16, marginBottom: 5}}>
-            <Text style={[styles(isDark).value, {fontFamily: 'Lato-Bold'}]}>
+
+          <View style={{ marginHorizontal: 16, marginBottom: 5 }}>
+            <Text style={[styles(isDark).value, { fontFamily: 'Lato-Bold' }]}>
               Regarding{' : '}
-              <Text style={[styles(isDark).value, {textAlign: 'left'}]}>
+              <Text style={[styles(isDark).value, { textAlign: 'left' }]}>
                 {feedbackByID[0]?.regardingTo?.label}
               </Text>
             </Text>
 
-            <Text style={[styles(isDark).value, {fontFamily: 'Lato-Bold'}]}>
+            <Text style={[styles(isDark).value, { fontFamily: 'Lato-Bold' }]}>
               Status{' : '}
               <Text style={styles(isDark).value}>
                 {feedbackByID[0]?.status?.label}
@@ -145,32 +144,32 @@ const ViewFeedback = ({route,navigation}: any) => {
           {(feedbackByID[0]?.status?.label === 'Declined' ||
             (feedbackByID[0]?.status?.label === 'Resolved' &&
               feedbackByID[0]?.publishToEmp === true)) && (
-            <>
-              <View style={styles(isDark).row}>
-                <View>
-                  <Text
-                    style={[styles(isDark).value, {fontFamily: 'Lato-Bold'}]}>
-                    Date of Resolution:
-                  </Text>
-                  <Text style={[styles(isDark).value, {textAlign: 'left'}]}>
-                    {feedbackByID[0]?.actionTakenOn}
-                  </Text>
-                </View>
+              <>
+                <View style={styles(isDark).row}>
+                  <View>
+                    <Text
+                      style={[styles(isDark).value, { fontFamily: 'Lato-Bold' }]}>
+                      Date of Resolution:
+                    </Text>
+                    <Text style={[styles(isDark).value, { textAlign: 'left' }]}>
+                      {feedbackByID[0]?.actionTakenOn}
+                    </Text>
+                  </View>
 
-                <View>
-                  <Text
-                    style={[styles(isDark).value, {fontFamily: 'Lato-Bold'}]}>
-                    Action taken by:
-                  </Text>
-                  <Text style={[styles(isDark).value]}>
-                    {feedbackByID[0]?.actionTakenBy}
-                  </Text>
+                  <View>
+                    <Text
+                      style={[styles(isDark).value, { fontFamily: 'Lato-Bold' }]}>
+                      Action taken by:
+                    </Text>
+                    <Text style={[styles(isDark).value]}>
+                      {feedbackByID[0]?.actionTakenBy}
+                    </Text>
+                  </View>
                 </View>
-              </View>
-            </>
-          )}
+              </>
+            )}
           <View style={styles(isDark).section}>
-            <Text style={[styles(isDark).value, {fontFamily: 'Lato-Bold'}]}>
+            <Text style={[styles(isDark).value, { fontFamily: 'Lato-Bold' }]}>
               Description:
             </Text>
             <Text style={styles(isDark).value}>
@@ -181,15 +180,15 @@ const ViewFeedback = ({route,navigation}: any) => {
           {(feedbackByID[0]?.status?.label === 'Declined' ||
             (feedbackByID[0]?.status?.label === 'Resolved' &&
               feedbackByID[0]?.publishToEmp === true)) && (
-            <View style={styles(isDark).section}>
-              <Text style={[styles(isDark).value, {fontFamily: 'Lato-Bold'}]}>
-                Resolution:
-              </Text>
-              <Text style={styles(isDark).value}>
-                {feedbackByID[0]?.resolution}
-              </Text>
-            </View>
-          )}
+              <View style={styles(isDark).section}>
+                <Text style={[styles(isDark).value, { fontFamily: 'Lato-Bold' }]}>
+                  Resolution:
+                </Text>
+                <Text style={styles(isDark).value}>
+                  {feedbackByID[0]?.resolution}
+                </Text>
+              </View>
+            )}
 
           {attachmentData?.data?.length > 0 && (
             <View style={styles(isDark).attachmentBox}>
@@ -198,7 +197,7 @@ const ViewFeedback = ({route,navigation}: any) => {
                   <Text
                     style={[
                       styles(isDark).value,
-                      {flexWrap: 'wrap', width: '70%'},
+                      { flexWrap: 'wrap', width: '70%' },
                     ]}>
                     {attachment.fileName}
                   </Text>
@@ -207,7 +206,8 @@ const ViewFeedback = ({route,navigation}: any) => {
                     size={40}
                     iconColor={Colors.primary}
                     onPress={() =>
-                      downloadFile(attachment.bytes, attachment.fileName)
+                      // downloadFile(attachment.bytes, attachment.fileName)
+                      handleDownload(attachment.bytes, attachment.fileName)
                     }
                   />
                 </View>
@@ -247,7 +247,7 @@ const styles = (isDark: boolean) =>
       fontFamily: 'Lato-Bold',
       color: Colors.primary,
       marginBottom: 8,
-      
+
     },
     value: {
       fontSize: 14,
